d0aa4c6c90b8d47aa3953f31a49b4fd5a36d7cd3
Rens Mulder <mulderrens@outlook.com>
2017-06-25 09:41:16 +0200

    Configuration Update

--- Afterlife/src/nl/gewoonhdgaming/afterlife/Main.java
+++ Afterlife/src/nl/gewoonhdgaming/afterlife/Main.java
@@ -8,17 +8,26 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import net.minecraft.server.v1_10_R1.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_10_R1.PacketPlayOutChat;
 
 public class Main extends JavaPlugin implements Listener {
 	
@@ -34,50 +43,24 @@ public class Main extends JavaPlugin implements Listener {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
-		if(command.getName().equalsIgnoreCase("afterlife")) {
-			if(sender.hasPermission("AfterLife.Admin")) {
-			if(args.length == 0) {
-				sender.sendMessage(ChatColor.RED + "Te weinig argumenten!");
-				return false;
-			}
-			if(Bukkit.getPlayer(args[0]) == null) {
-				sender.sendMessage(ChatColor.RED + "Speler bestaat niet!");
-				return false;
-			}
-			Player p = Bukkit.getPlayer(args[0]);
-			sender.sendMessage(ChatColor.AQUA + "Speler is in afterlife gezet!");
-			afterlife.put(Bukkit.getPlayer(args[0]).getUniqueId(), 300);//Default = 300
-			p.setGameMode(GameMode.ADVENTURE);
-            p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, Integer.MAX_VALUE, false, false));
-            p.setCanPickupItems(false);
-            p.sendTitle(ChatColor.AQUA + "U bent dood gegaan", ChatColor.RED + "U bent tijdelijk een geest");
-            p.sendMessage(ChatColor.RED + "Je bent nu 5 minuten niet zichbaar voor andere spelers, ook kan je niet catten, items opakken of commands uitvoeren. Na 5 minuten zal je weer levend zijn en kan je alles weer!");
-            Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "mute " + p.getName() + " 5m"); //default = 5m
-            Bukkit.getOnlinePlayers().forEach((otherPlayer) -> otherPlayer.hidePlayer(p));
-            p.setGameMode(GameMode.SURVIVAL);
-            p.setCanPickupItems(true);
-			 new BukkitRunnable() {
-					
-					@Override
-					public void run() {
-						if(!afterlife.get(p.getUniqueId()).equals(0)) {
-							afterlife.put(p.getUniqueId(), afterlife.get(p.getUniqueId()) - 1);
-						} else {
-							afterlife.remove(p.getUniqueId());
-							 p.removePotionEffect(PotionEffectType.INVISIBILITY);
-                             Bukkit.getOnlinePlayers().forEach((otherPlayer) -> otherPlayer.showPlayer(p));
-                             p.sendMessage(ChatColor.AQUA + "Je bent weer levend! Veel succes.");
-							this.cancel();
-						}
-					}
-				}.runTaskTimerAsynchronously(this, 20, 20);
-       
-			
-			} else {
-				sender.sendMessage(ChatColor.RED + "Niet genoeg permissie!");
-			}
-		}
-    	else if (command.getName().equalsIgnoreCase("al") && sender instanceof Player){
+//		if(command.getName().equalsIgnoreCase("afterlife")) {
+//			if(sender.hasPermission("AfterLife.Admin")) {
+//			if(args.length == 0) {
+//				sender.sendMessage(ChatColor.RED + "Te weinig argumenten!");
+//				return false;
+//			}
+//			if(Bukkit.getPlayer(args[0]) == null) {
+//				sender.sendMessage(ChatColor.RED + "Speler bestaat niet!");
+//				return false;
+//			}
+//			
+//       
+//			
+//			} else {
+//				sender.sendMessage(ChatColor.RED + "Niet genoeg permissie!");
+//			}
+//		}
+    	 if (command.getName().equalsIgnoreCase("al") && sender instanceof Player){
 			
 			Player p = (Player) sender;
 			if (args.length == 0) {
@@ -100,6 +83,46 @@ public class Main extends JavaPlugin implements Listener {
 		return false;	
 		
 	}
+	@EventHandler
+	public void onDeath(PlayerDeathEvent e) {
+		if(!(e.getEntity() instanceof Player)) {
+			return;
+		}
+		Player p = e.getEntity();
+		afterlife.put(p.getUniqueId(), 300);//Default = 300
+		p.setGameMode(GameMode.ADVENTURE);
+        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, Integer.MAX_VALUE, false, false));
+        p.setCanPickupItems(false);
+        p.sendTitle(ChatColor.AQUA + "U bent dood gegaan", ChatColor.RED + "U bent tijdelijk een geest");
+        p.sendMessage(ChatColor.RED + "Je bent nu 5 minuten niet zichbaar voor andere spelers, ook kan je niet chatten, items opakken of commands uitvoeren. Na 5 minuten zal je weer levend zijn en kan je alles weer!");
+        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "mute " + p.getName() + " 5m"); //default = 5m
+        Bukkit.getOnlinePlayers().forEach((otherPlayer) -> otherPlayer.hidePlayer(p));
+        p.setGameMode(GameMode.SURVIVAL);
+        p.setCanPickupItems(true);
+		 new BukkitRunnable() {
+				
+				@Override
+				public void run() {
+					if(!afterlife.get(p.getUniqueId()).equals(0)) {
+						afterlife.put(p.getUniqueId(), afterlife.get(p.getUniqueId()) - 1);
+					} else {
+						afterlife.remove(p.getUniqueId());
+						 p.removePotionEffect(PotionEffectType.INVISIBILITY);
+                         Bukkit.getOnlinePlayers().forEach((otherPlayer) -> otherPlayer.showPlayer(p));
+                         p.sendMessage(ChatColor.AQUA + "Je bent weer levend! Veel succes.");
+						this.cancel();
+					}
+					int i = afterlife.get(p.getUniqueId());
+					sendAction(p, ChatColor.WHITE + "Je bent nog voor " + ChatColor.AQUA + i + ChatColor.WHITE + " secondes een geest!");
+				}
+			}.runTaskTimerAsynchronously(this, 20, 20);
+	}
+	
+	public void sendAction(Player p, String message) {
+		PacketPlayOutChat packet = new PacketPlayOutChat(ChatSerializer.a("{\"text\":\"" + message + "\"}"), (byte) 2);
+		((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+	}
+	
 	@EventHandler
 	public void chatEvent(AsyncPlayerChatEvent e) {
 		if(afterlife.containsKey(e.getPlayer())) {
@@ -127,6 +150,61 @@ public class Main extends JavaPlugin implements Listener {
 					+ afterlife.get(e.getPlayer().getUniqueId()) + " secondes in afterlive!");
 			e.setCancelled(true);
 		}
+	}
+	
+	@EventHandler
+	public void onDisconnect(PlayerQuitEvent e) {
+		if(afterlife.containsKey(e.getPlayer().getUniqueId())) {
+			getConfig().set("sessions." + e.getPlayer().getUniqueId(), afterlife.get(e.getPlayer().getUniqueId()));
+			saveConfig();
+			afterlife.remove(e.getPlayer().getUniqueId());
+		}
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		if(getConfig().get("sessions." + e.getPlayer().getUniqueId()) != null || getConfig().getInt("sessions." + e.getPlayer().getUniqueId()) != 0 || getConfig().getInt("sessions." + e.getPlayer().getUniqueId()) > 0) {
+			Player p = (Player) e.getPlayer();
+			afterlife.put(p.getUniqueId(), getConfig().getInt("sessions." + e.getPlayer().getUniqueId()));//Default = 300
+			p.setGameMode(GameMode.ADVENTURE);
+	        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, Integer.MAX_VALUE, false, false));
+	        p.setCanPickupItems(false);
+	        p.sendTitle(ChatColor.AQUA + "U bent dood gegaan", ChatColor.RED + "U bent tijdelijk een geest");
+	        p.sendMessage(ChatColor.RED + "Je bent nu " + getConfig().get("sessions." + e.getPlayer().getUniqueId()) + " secondes niet zichbaar voor andere spelers, ook kan je niet chatten, items opakken of commands uitvoeren. Na 5 minuten zal je weer levend zijn en kan je alles weer!");
+	        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "mute " + p.getName() + getConfig().getInt("sessions." + e.getPlayer().getUniqueId()) + "s"); //default = 5m
+	        Bukkit.getOnlinePlayers().forEach((otherPlayer) -> otherPlayer.hidePlayer(p));
+	        p.setGameMode(GameMode.SURVIVAL);
+	        p.setCanPickupItems(true);
+			 new BukkitRunnable() {
+					
+					@Override
+					public void run() {
+						if(!afterlife.get(p.getUniqueId()).equals(0)) {
+							afterlife.put(p.getUniqueId(), afterlife.get(p.getUniqueId()) - 1);
+						} else {
+							afterlife.remove(p.getUniqueId());
+							 p.removePotionEffect(PotionEffectType.INVISIBILITY);
+	                         Bukkit.getOnlinePlayers().forEach((otherPlayer) -> otherPlayer.showPlayer(p));
+	                         p.sendMessage(ChatColor.AQUA + "Je bent weer levend! Veel succes.");
+							this.cancel();
+						}
+						int i = afterlife.get(p.getUniqueId());
+						sendAction(p, ChatColor.WHITE + "Je bent nog voor " + ChatColor.AQUA + i + ChatColor.WHITE + " secondes een geest!");
+					}
+				}.runTaskTimerAsynchronously(this, 20, 20);
+		}
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e) {
+		Player p = e.getPlayer();
+		if(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.LEFT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_AIR) {
+			if(afterlife.containsKey(p.getUniqueId())) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "Je zit in de afterlive, dus kan je niet interacten! Je bent nog voor "
+						+ afterlife.get(e.getPlayer().getUniqueId()) + " secondes in afterlive!");
+			}
+         }
 	}
 
 }
